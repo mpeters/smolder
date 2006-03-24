b8766dccb86d00d630d482dae3466a01cc8fb9a6
Michael Peters <mpeters@plusthree.com>
2006-03-24 00:28:19 +0000

    need to reverse the order of this insert

--- sql/insert.sql
+++ sql/insert.sql
@@ -1,2 +1,2 @@
-INSERT INTO developer (id, username, fname, lname, email, password, admin, preference) VALUES (1, 'admin', 'Joe', 'Admin', 'test@test.com', 'YhKDbhvT1LKkg', 1, 1);
 INSERT INTO preference (id) VALUES (1);
+INSERT INTO developer (id, username, fname, lname, email, password, admin, preference) VALUES (1, 'admin', 'Joe', 'Admin', 'test@test.com', 'YhKDbhvT1LKkg', 1, 1);
