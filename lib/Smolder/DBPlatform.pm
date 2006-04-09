957ec8a4c8aca85ad4559dd5aac9d808b198dc7e
Michael Peters <mpeters@plusthree.com>
2006-04-09 20:35:37 +0000

    adding documentation

--- lib/Smolder/DBPlatform.pm
+++ lib/Smolder/DBPlatform.pm
@@ -85,130 +85,220 @@ user.
 This should search for any required shared object files and header files
 using L<Smolder::Platform>'s <check_libs()> method.
 
-    $platform->verify_dependencies(mode => 'install')
+    $db_platform->verify_dependencies(mode => 'install')
 
 =cut
 
 sub verify_dependencies { 
     my $class = shift;
-    die "verify_dependencies() must be implemented in the $class";
+    die "verify_dependencies() must be implemented in $class";
 }
 
 =head2 verify_admin
+
+Given the password, hostname and database name, returns true if those credentials
+can be used for administrative access to the database.
+
+    $db_platform->verify_admin(
+        passwd  => $pw,
+        host    => 'localhost',
+        db_name => 'smolder',
+    );
 
 =cut
 
 sub verify_admin { 
     my $class = shift;
-    die "verify_admin() must be implemented in the $class";
+    die "verify_admin() must be implemented in $class";
 }
 
 =head2 run_sql_file
+
+Given the DB username, password, hostname, database name and full file name, runs the SQL
+contained in the file.
+
+    $db_platform->run_sql_file(
+        user    => $user,
+        passwd  => $pw,
+        host    => $host,
+        db_name => 'smolder',
+        file    => '/usr/local/smolder/foo.sql',
+    );
 
 =cut
 
 sub run_sql_file { 
     my $class = shift;
-    die "run_sql_file() must be implemented in the $class";
+    die "run_sql_file() must be implemented in $class";
 }
 
 =head2 dbh
+
+Given the username, password, database name and host name will return the appropriate 
+database handle.
+
+    my $dbh = $db_platform->dbh(
+        user    => 'smolder',
+        passwd  => 's3cr3t',
+        db_name => 'smolder',
+        host    => 'localhost',
+    );
 
 =cut
 
 sub dbh { 
     my $class = shift;
-    die "dbh() must be implemented in the $class";
+    die "dbh() must be implemented in $class";
 }
 
 =head2 dbi_driver
+
+Returns the class name of the C<DBI> driver that this DBPlatform module uses.
 
 =cut
 
 sub dbi_driver { 
     my $class = shift;
-    die "dbi_driver() must be implemented in the $class";
+    die "dbi_driver() must be implemented in $class";
 }
 
 =head2 cdbi_class
+
+Returns the class name of the C<Class::DBI> driver that this DBPlatform module uses.
 
 =cut
 
 sub cdbi_class { 
     my $class = shift;
-    die "cdbi_class() must be implemented in the $class";
+    die "cdbi_class() must be implemented in $class";
 }
 
 =head2 dump_database
+
+Given the filename of where to put the dump, this method will create the SQL necessary
+to restore the database to it's present state including all schema creationg statements.
+
+    $db_platform->dump_database('/usr/local/smolder/dump.sql');
 
 =cut
 
 sub dump_database { 
     my $class = shift;
-    die "dump_database() must be implemented in the $class";
+    die "dump_database() must be implemented in $class";
 }
 
 =head2 drop_database
+
+Given the password of the database admin user, the hostname and database name, will drop the
+existing database.
+
+    $db_platform->drop_database(
+        admin_password => 's3cr3t',
+        host           => 'localhost',
+        db_name        => 'smolder',
+    );
 
 =cut
 
 sub drop_database {
     my $class = shift;
-    die "drop_database() must be implemented in the $class";
+    die "drop_database() must be implemented in $class";
 }
 
 =head2 create_database
+
+Given the password of the database admin user, the hostname and database name, will create 
+the initial database.
+
+    $db_platform->create_database(
+        admin_password => 's3cr3t',
+        host           => 'localhost',
+        db_name        => 'smolder',
+    );
 
 =cut
 
 sub create_database {
     my $class = shift;
-    die "create_database() must be implemented in the $class";
+    die "create_database() must be implemented in $class";
 }
 
 =head2 create_user
+
+Given the password of the database admin user, the hostname, the database name, the new user's
+username and password, will create a new database user with access to the database.
+
+    $db_platform->create_user(
+        admin_passwd => $admin_password,
+        host         => $db_host,
+        db_name      => $db_name,
+        user         => $db_user,
+        passwd       => $db_pass,
+    );
 
 =cut
 
 sub create_user { 
     my $class = shift;
-    die "create_user() must be implemented in the $class";
+    die "create_user() must be implemented in $class";
 }
 
 =head2 sql_create_dir
+
+Returns the full path to the directory containing the SQL files for the creation
+of the database for the particular DB driver being used.
 
 =cut
 
 sub sql_create_dir { 
     my $class = shift;
-    die "sql_create_dir() must be implemented in the $class";
+    die "sql_create_dir() must be implemented in $class";
 }
 
 =head2 sql_upgrade_dir
+
+Returns the full path to the directory containing the SQL files for the upgrade
+of the database for the particular DB driver being used.
 
 =cut
 
 sub sql_upgrade_dir { 
     my $class = shift;
-    die "sql_upgrade_dir() must be implemented in the $class";
+    die "sql_upgrade_dir() must be implemented in $class";
 }
 
 =head2 get_enum_values
+
+Given the table and column names will return all legal values if that column
+is an ENUM type as an arrayref.
+
+    my $values = $db_platform->get_enum_values(
+        table  => $table,
+        column => $column,
+    );
 
 =cut
 
 sub get_enum_values { 
     my $class = shift;
-    die "get_enum_values() must be implemented in the $class";
+    die "get_enum_values() must be implemented in $class";
 }
 
 =head2 unique_failure_msg 
+
+Given a DB failure message, will return true if the message was a failure due to a
+failed UNIQUE contstraint, else will return false.
+
+    eval { $class->create(%args) };
+    if( $@ ) {
+        die unless $db_platform->unique_failure_msg($@);
+    }
 
 =cut
 
 sub unique_failure_msg { 
     my $class = shift;
-    die "unique_failure_msg() must be implemented in the $class";
+    die "unique_failure_msg() must be implemented in $class";
 }
 
 
