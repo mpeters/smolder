1f4fd49f2a56ea1f818d3d3ed3031c2d8f8838be
Michael Peters <mpeters@plusthree.com>
2006-08-16 16:19:37 +0000

    adding the guest account on creation

--- sql/insert.sql
+++ sql/insert.sql
@@ -1,5 +1,7 @@
-INSERT INTO db_version VALUES ("0.2");
+INSERT INTO db_version VALUES ("0.3");
 
 INSERT INTO preference (id) VALUES (1);
+INSERT INTO preference (id) VALUES (2);
 
-INSERT INTO developer (id, username, fname, lname, email, password, admin, preference) VALUES (1, 'admin', 'Joe', 'Admin', 'test@test.com', 'YhKDbhvT1LKkg', 1, 1);
+INSERT INTO developer (id, username, fname, lname, email, password, admin, preference, guest) VALUES (1, 'admin', 'Joe', 'Admin', 'test@test.com', 'YhKDbhvT1LKkg', 1, 1, 0);
+INSERT INTO developer (id, username, fname, lname, email, password, admin, preference, guest) VALUES (2, 'anonymous', '', '', '', '', 0, 2, 1);
