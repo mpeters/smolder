95c47f06abdca8e7ff682278eadca81fe1ec7ec3
Michael Peters <mpeters@plusthree.com>
2006-08-16 16:22:04 +0000

    we need to update the failed value from fail

--- upgrades/sql/mysql/V0_3.sql
+++ upgrades/sql/mysql/V0_3.sql
@@ -5,3 +5,4 @@ ALTER TABLE preference ADD COLUMN email_sent_timestamp DATETIME;
 ALTER TABLE developer ADD COLUMN guest BOOL NOT NULL DEFAULT 0;
 
 ALTER TABLE smoke_report ADD COLUMN failed BOOLEAN NOT NULL DEFAULT 0;
+UPDATE smoke_report SET failed = ( fail > 1);
--- upgrades/sql/sqlite/V0_3.sql
+++ upgrades/sql/sqlite/V0_3.sql
@@ -5,3 +5,4 @@ ALTER TABLE preference ADD COLUMN email_sent_timestamp INTEGER;
 ALTER TABLE developer ADD COLUMN guest INTEGER DEFAULT 0;
 
 ALTER TABLE smoke_report ADD COLUMN failed INTEGER DEFAULT 0;
+UPDATE smoke_report SET failed = ( fail > 1);
