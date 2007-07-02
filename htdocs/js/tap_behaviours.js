7dfe28a34eb0b48e73c788904f322c1a9055f39e
Michael Peters <mpeters@plusthree.com>
2007-07-02 01:12:18 +0000

    add "show all" link to test reports

--- htdocs/js/tap_behaviours.js
+++ htdocs/js/tap_behaviours.js
@@ -41,7 +41,43 @@ var myrules = {
     'td.tooltip_trigger' : function(el) {
         var diag = document.getElementsByClassName('tooltip', el)[0];
         setup_tooltip(el, diag);
+    },
+    'a.show_all' : function(el) {
+        Event.observe(el, 'click', function() {
+            // an anonymous function that we use as a callback after each
+            // panel is opened so that it opens the next one
+            var show_details = function(index) {
+                var el = $('testfile_details_' + index);
+                // return if we're the last one
+                if( ! el ) return;
+
+                // we only need to fetch it if we're not already visible
+                if( Element.visible(el) ) {
+                    show_details(++index);
+                } else {
+                    matches = el.id.match(/^testfile_details_(\d+)$/);
+                    var index = matches[1];
+                    var indicator = $('indicator_' + index);
+                    var div = $('testfile_details_' + index);
+                    indicator.style.visibility = 'visible';
+                    
+                    ajax_submit({
+                        url        : $('for_testfile_details_' + index).href,
+                        div        : div,
+                        indicator  : 'none',
+                        onComplete : function() {
+                            Element.show(div);
+                            indicator.style.visibility = 'hidden';
+                            show_details(++index);
+                        }
+                    });
+                }
+            };
+            show_details(0);
+            Behaviour.apply();
+        });
     }
 };
 
 Behaviour.register(myrules);
+
