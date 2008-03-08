a41d493cb36d467f4b54a6625d0ffbf42e84c23c
Michael Peters <mpeters@plusthree.com>
2008-03-08 20:37:50 +0000

    updating for F8

---
+++ platform/Fedora8/Platform.pm
@@ -0,0 +1,18 @@
+package Fedora8::Platform;
+use strict;
+use warnings;
+
+use base 'FC1::Platform';
+
+use Cwd qw(cwd);
+
+sub guess_platform {
+    return 0 unless -e '/etc/redhat-release';
+    open(RELEASE, '/etc/redhat-release') or return 0;
+    my $release = <RELEASE>;
+    close RELEASE;
+    return 1 if $release =~ /Fedora release 8/;
+    return 0;
+}
+
+1;
