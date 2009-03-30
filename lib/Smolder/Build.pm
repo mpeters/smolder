92f6de418c976fe46030829f012c789733170e84
Michael Peters <mpeters@plusthree.com>
2009-03-30 00:29:45 +0000

    make sure we dont re-run the build-code target by making it a no-op

--- lib/Smolder/Build.pm
+++ lib/Smolder/Build.pm
@@ -71,7 +71,16 @@ sub _wrap_test_action {
     }
 
     my $method = "SUPER::ACTION_$action";
-    $self->$method(@_);
+    {
+        # make sure depends_on('code') doesn't get run since we've already taken care of it
+        local *Module::Build::TAPArchive::depends_on = sub {
+            my ($self, @args) = @_;
+            if($args[0] && $args[0] ne 'code') {
+                return $self->SUPER::depends_on(@args);
+            }
+        };
+        $self->$method(@_);
+    }
     
     # finish() seems to hang, so just kill it
     $subprocess->kill_kill;
