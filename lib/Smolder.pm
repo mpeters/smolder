a73a7dbd757311d37899aa7b8686e93a434d3383
Michael Peters <mpeters@plusthree.com>
2009-03-30 21:32:06 +0000

    adding more info about bugs, etc to main Smolder.pm POD

--- lib/Smolder.pm
+++ lib/Smolder.pm
@@ -21,3 +21,35 @@ Start your smolder server:
 If you have a better idea for what Smolder is and how I can describe it, I'm all ears :)
 
 Please see L<Smolder::Manual> for how to use it.
+
+=head2 SUPPORT
+
+=over
+
+=item * Bugs and Requests
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Smolder>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Smolder>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Smolder>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Smolder/>
+
+=back
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Michael Peters, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
+
