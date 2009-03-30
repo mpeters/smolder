877746607f14ed5c04ca82e0ee617e734cb19644
Michael Peters <mpeters@plusthree.com>
2009-03-30 00:05:49 +0000

    give it a little POD top make Module::Build happy and give it an actual home page on search.cpan.org

--- lib/Smolder.pm
+++ lib/Smolder.pm
@@ -4,3 +4,21 @@ use File::Spec::Functions qw(catdir);
 our $VERSION = '1.30';
 
 1;
+
+__END__
+
+=head1 NAME 
+
+Smolder - Web-based smoke test aggregator
+
+=head1 SYNOPSIS
+
+Start your smolder server:
+
+    ]$ smolder
+
+=head1 DESCRIPTION
+
+If you have a better idea for what Smolder is and how I can describe it, I'm all ears :)
+
+Please see L<Smolder::Manual> for how to use it.
