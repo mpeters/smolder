786b5ad6b4b5ae8c7945036f163339d60a14b390
Jonathan Swartz <swartz@pobox.com>
2009-07-14 06:06:33 +0800

    debug utilities; don't need to be in final product
    Signed-off-by: mpeters <mpeters@plusthree.com>

---
+++ lib/Smolder/Debug.pm
@@ -0,0 +1,52 @@
+package Smolder::Debug;
+use Carp qw(longmess);
+use strict;
+use warnings;
+use base qw(Exporter);
+
+our @EXPORT = qw(
+  dd
+  dp
+  dps
+  dpo
+  dpso
+);
+
+sub dump_one_line {
+    my ($value) = @_;
+
+    return Data::Dumper->new( [$value] )->Indent(0)->Sortkeys(1)->Quotekeys(0)
+      ->Terse(1)->Dump();
+}
+
+sub _dump_value_with_caller {
+    my ($value) = @_;
+
+    my $dump =
+      Data::Dumper->new( [$value] )->Indent(1)->Sortkeys(1)->Quotekeys(0)
+      ->Terse(1)->Dump();
+    my @caller = caller(1);
+    return sprintf( "[dp at %s line %d.] %s\n", $caller[1], $caller[2], $dump );
+}
+
+sub dd {
+    die _dump_value_with_caller(@_);
+}
+
+sub dp {
+    print STDERR _dump_value_with_caller(@_);
+}
+
+sub dps {
+    print STDERR longmess( _dump_value_with_caller(@_) );
+}
+
+sub dpo {
+    print _dump_value_with_caller(@_);
+}
+
+sub dpso {
+    print longmess( _dump_value_with_caller(@_) );
+}
+
+1;
