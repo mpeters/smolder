2f331282fee4c598d921d7c92163203d600df050
Jonathan Swartz <swartz@pobox.com>
2009-07-15 02:33:55 +0800

    rename index properly
    Signed-off-by: Michael Peters <mpeters@plusthree.com>

--- sql/test_file.sql
+++ sql/test_file.sql
@@ -6,4 +6,4 @@ CREATE TABLE test_file  (
     CONSTRAINT 'fk_smoke_report_project' FOREIGN KEY ('project') REFERENCES 'project' ('id') ON DELETE CASCADE
 );
 
-CREATE INDEX i_project_test_file ON test_file (project);
+CREATE INDEX i_test_file_project ON test_file (project);
