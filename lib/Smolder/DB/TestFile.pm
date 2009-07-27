79b6d6527de8372591ce7140bd3f0f3b60392749
Jonathan Swartz <swartz@pobox.com>
2009-07-28 05:15:45 +0800

    convert mute_until to and from DateTime; add is_muted
    Signed-off-by: Michael Peters <mpeters@plusthree.com>

--- lib/Smolder/DB/TestFile.pm
+++ lib/Smolder/DB/TestFile.pm
@@ -5,6 +5,12 @@ use base 'Smolder::DB';
 
 __PACKAGE__->set_up_table('test_file');
 
+__PACKAGE__->has_a(
+    mute_until => 'DateTime',
+    inflate    => sub { DateTime->from_epoch(epoch => shift) },
+    deflate => sub { shift->epoch },
+);
+
 =head1 NAME
 
 Smolder::DB::TestFile
@@ -17,11 +23,19 @@ L<Class::DBI> based model class for the 'test_file' table in the database.
 
 =head2 ACCESSSOR/MUTATORS
 
-Each column in the borough table has a method with the same name
-that can be used as an accessor and mutator.
+Each column in the borough table has a method with the same name that can be
+used as an accessor and mutator.
 
 =cut
-    
+
+sub is_muted {
+    my ($self) = @_;
+
+    my $mute_until = $self->mute_until;
+    my $is_muted = defined($mute_until) && time < $mute_until->epoch;
+    return $is_muted;
+}
+
 __PACKAGE__->has_a(project => 'Smolder::DB::Project');
 
 1;
