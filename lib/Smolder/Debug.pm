e7d6bbdba1bc6aa354529bcc670516a73f838373
Jonathan Swartz <swartz@pobox.com>
2009-07-28 05:16:51 +0800

    tidy
    Signed-off-by: Michael Peters <mpeters@plusthree.com>

--- lib/Smolder/Debug.pm
+++ lib/Smolder/Debug.pm
@@ -1,5 +1,6 @@
 package Smolder::Debug;
 use Carp qw(longmess);
+use Data::Dumper;
 use strict;
 use warnings;
 use base qw(Exporter);
@@ -15,18 +16,15 @@ our @EXPORT = qw(
 sub dump_one_line {
     my ($value) = @_;
 
-    return Data::Dumper->new( [$value] )->Indent(0)->Sortkeys(1)->Quotekeys(0)
-      ->Terse(1)->Dump();
+    return Data::Dumper->new([$value])->Indent(0)->Sortkeys(1)->Quotekeys(0)->Terse(1)->Dump();
 }
 
 sub _dump_value_with_caller {
     my ($value) = @_;
 
-    my $dump =
-      Data::Dumper->new( [$value] )->Indent(1)->Sortkeys(1)->Quotekeys(0)
-      ->Terse(1)->Dump();
+    my $dump = Data::Dumper->new([$value])->Indent(1)->Sortkeys(1)->Quotekeys(0)->Terse(1)->Dump();
     my @caller = caller(1);
-    return sprintf( "[dp at %s line %d.] %s\n", $caller[1], $caller[2], $dump );
+    return sprintf("[dp at %s line %d.] %s\n", $caller[1], $caller[2], $dump);
 }
 
 sub dd {
@@ -38,7 +36,7 @@ sub dp {
 }
 
 sub dps {
-    print STDERR longmess( _dump_value_with_caller(@_) );
+    print STDERR longmess(_dump_value_with_caller(@_));
 }
 
 sub dpo {
@@ -46,7 +44,7 @@ sub dpo {
 }
 
 sub dpso {
-    print longmess( _dump_value_with_caller(@_) );
+    print longmess(_dump_value_with_caller(@_));
 }
 
 1;
