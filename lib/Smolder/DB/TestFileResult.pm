6626e2880fa511964b33cc0a26ac474825793833
Jonathan Swartz <swartz@pobox.com>
2009-07-28 05:16:06 +0800

    tidy; add insert_or_replace
    Signed-off-by: Michael Peters <mpeters@plusthree.com>

--- lib/Smolder/DB/TestFileResult.pm
+++ lib/Smolder/DB/TestFileResult.pm
@@ -11,18 +11,34 @@ Smolder::DB::TestFileResult
 
 =head1 DESCRIPTION
 
-L<Class::DBI> based model class for the 'test_file_result' table in the database.
+L<Class::DBI> based model class for the 'test_file_result' table in the
+database.
 
 =head1 METHODS
 
 =head2 ACCESSSOR/MUTATORS
 
-Each column in the borough table has a method with the same name
-that can be used as an accessor and mutator.
+Each column in the borough table has a method with the same name that can be
+used as an accessor and mutator.
 
 =cut
-    
-__PACKAGE__->has_a(test_file => 'Smolder::DB::TestFile');
+
+sub insert_or_replace {
+    my ($class, $params) = @_;
+
+    if (
+        my $result = $class->retrieve(
+            test_file    => $params->{test_file},
+            smoke_report => $params->{smoke_report}
+        )
+      )
+    {
+        $result->delete();
+    }
+    $class->insert($params);
+}
+
+__PACKAGE__->has_a(test_file    => 'Smolder::DB::TestFile');
 __PACKAGE__->has_a(smoke_report => 'Smolder::DB::SmokeReport');
 
 1;
