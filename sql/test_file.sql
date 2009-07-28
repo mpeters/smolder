77fb2a7d077918f68a88cd39b31c449afb227f16
Jonathan Swartz <swartz@pobox.com>
2009-07-29 02:34:08 +0800

    fix constraint name
    Signed-off-by: Michael Peters <mpeters@plusthree.com>

--- sql/test_file.sql
+++ sql/test_file.sql
@@ -3,7 +3,7 @@ CREATE TABLE test_file  (
     project         INTEGER NOT NULL, 
     label           TEXT DEFAULT '',
     mute_until      INTEGER,
-    CONSTRAINT 'fk_smoke_report_project' FOREIGN KEY ('project') REFERENCES 'project' ('id') ON DELETE CASCADE
+    CONSTRAINT 'fk_test_file_project' FOREIGN KEY ('project') REFERENCES 'project' ('id') ON DELETE CASCADE
 );
 
 CREATE INDEX i_test_file_project ON test_file (project);
