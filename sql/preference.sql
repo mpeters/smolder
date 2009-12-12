8104a15d281edb22d97ffb2b46976c56f0f1cb2d
Aaron Schrab <aaron@schrab.com>
2009-12-11 19:38:06 -0500

    Remove extra comma.

--- sql/preference.sql
+++ sql/preference.sql
@@ -5,7 +5,7 @@ CREATE TABLE preference (
     email_limit INT DEFAULT 0,
     email_sent  INT DEFAULT 0,
     email_sent_timestamp INTEGER,
-    show_passing INT DEFAULT 1,
+    show_passing INT DEFAULT 1
 );
 
 INSERT INTO preference (id) VALUES (1);
