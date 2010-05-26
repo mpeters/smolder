f53d48f9523c94061d022cee97cb8a8294e4727f
David Raynes <rayners@gmail.com>
2010-05-26 17:14:07 -0400

    Ran through tidy before making the redirect change

--- lib/Smolder/Redirect.pm
+++ lib/Smolder/Redirect.pm
@@ -5,14 +5,14 @@ use base 'CGI::Application';
 
 sub setup {
     my $self = shift;
-    $self->run_modes(['redirect']);
+    $self->run_modes( ['redirect'] );
     $self->start_mode('redirect');
 }
 
 sub redirect {
     my $self = shift;
     $self->header_type('redirect');
-    $self->header_add(-uri => '/app');
+    $self->header_add( -uri => '/app' );
     return "Redirecting...\n";
 }
 
